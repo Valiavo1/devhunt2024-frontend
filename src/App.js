bab848d482001e4439c6fe65e4b3f4bd795f9348
Valiavo1 <valiavoandriantsoa30@gmail.com>
2024-03-05 16:45:50 +0300

    remove logo

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>DEV MODIFICATION</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
